86d42d53c4a49a606748c11ed6728de3c9e26f22
Jason Frerich <jasonfrerich@yahoo.com>
2019-01-11 11:28:20 -0600

    More polishing

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.URLField()
+    image = models.ImageField(upload_to="images/")
+    icon = models.ImageField(upload_to="images/")
+    votes_total = models.IntegerField(default=1)
+
+    pub_date_pretty = models.CharField()
+
+    # Foreign key is id from other model
+    # Foreign key points to User Id from User models (db)
+    # if user deletes their account, also delete their products
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100] + '...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
